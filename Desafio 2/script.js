6ca9b4238109db089aa9727403569c81ed1b8a69
KanjiSaiko <hbortolomiol2@gmail.com>
2024-02-29 03:17:04 -0300

    Segundo Desafio

---
+++ Desafio 2/script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle('light')
+}
+
+function switchbg() {
+    const html = document.documentElement
+    html.classList.toggle('bg-color')
+}
+
+function switchbgbuttom() {
+    const html = document.documentElement
+    html.classList.toggle('bg-botoes')
+}
